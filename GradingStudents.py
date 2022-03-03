707bb546301d21a5757bfcec21d82f2fa4ec4451
Luis Angel Herrera <luismaldonado25@outlook.com>
2022-03-02 22:10:33 -0600

    Simplificar los if

--- GradingStudents.py
+++ GradingStudents.py
@@ -4,18 +4,11 @@ from operator import mod
 def gradingStudents(grades):
     nums = []
     for num in grades:
-        if num >= 38:
-            if num % 5 == 0:
+        if num % 5 == 0 or (5-(num%5))>2 or num<38:
                 nums.append(num)
-            else:
-                multiple = num + (5-(num%5))
-                print(multiple)
-                if (multiple-num < 3):
-                    nums.append(multiple)
-                else:
-                    nums.append(num)    
         else:
-            nums.append(num)
+            multiple = num + (5-(num%5))
+            nums.append(multiple) 
     return(nums)        
 
 
@@ -23,5 +16,5 @@ def gradingStudents(grades):
 
 
 if __name__ =="__main__":
-    grades = [58,37,65,45,49,52,80,78]
+    grades = [58,37,65,45,49,52,80,78,37,48,69]
     print(gradingStudents(grades))
